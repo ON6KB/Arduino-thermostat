7e214a4bfa0cf490e5fe09cbaaad94a8fb007fde
ON6KB <81462181+ON6KB@users.noreply.github.com>
2021-05-05 11:19:07 +0200

    Update thermostat.ino

--- thermostat.ino
+++ thermostat.ino
@@ -3,6 +3,7 @@ Capteur de température DS18B20.
 Module 2 relais 5V
 Encodeur rotatif KY-040
 Ecran LCD I2C
+Bouton poussoir menu
 
 */
 
@@ -31,22 +32,27 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 // Constante des relais
-int const relais1 = 2;
-int const relais2 = 3;
+int const relais1 = 7;
+int const relais2 = 8;
+
+//bouton menu
+int const bouton_menu = 2;
 
 //CLK KY-040 sur D4
 int const pinCLK = 4;
 //DT KY-040 sur D5
 int const pinDT = 5;
-//bouton
+//bouton KY-040
 int const pinSW = 6;
+int boutonSW = 0;
+int debounce = 10;
 //Compteur position encodeur rotatif
 int Compteur = 0;
 //dernière valeur de CLK
 int pinCLKLast;
+//valeur actuel de CLK
 int clkVal;
-boolean Direction;
-boolean menu_principal = false;
+volatile boolean menu_principal = false;
 boolean marche_select = true;
 boolean tempMin_select = false;
 boolean tempMax_select = false;
@@ -76,10 +82,16 @@ void setup() {
   //Pin du relais
   pinMode (relais1, OUTPUT);
   pinMode (relais2, OUTPUT);
+  //pin bouton menu
+  pinMode(bouton_menu, INPUT_PULLUP);
+  //interruption sur D2
+  attachInterrupt(0,actionMenu, FALLING);
   //pin du KY-040
   pinMode (pinCLK, INPUT_PULLUP);
   pinMode (pinDT, INPUT_PULLUP);
   pinMode (pinSW, INPUT_PULLUP);
+  //init pinCLKLast
+  pinCLKLast = digitalRead(pinCLK);
   // Démarage du capteur de température
   sensors.begin();
   //démarrage du LCD
@@ -145,15 +157,96 @@ void chargeEEPROM() {
  // Sauvegarde les nouvelles données
   sauvegardeEEPROM();
 }
+void rotation (){
+  //Lecture de l'état de pinCLK
+  clkVal = digitalRead(pinCLK);
+  //Vérifie si l'encodeur a tourné (changement d'état)
+  if (clkVal != pinCLKLast){
+    if (digitalRead(pinDT) != clkVal) {
+      //pinCLK a changé en premier
+      Compteur ++;
+    }
+    else {
+      //pinDT a changé en premier
+      Compteur --;
+    }
+    pinCLKLast = clkVal ;
+  }
+  
+}
+void encodeur_menu () {
+//retour à 2 si compteur<0
+if (Compteur<0){
+  Compteur = 2;
+}
+
+//retour à 0 si compteur>2
+else if (Compteur>2){
+  Compteur = 0;
+  
+}
+
+else {
+  sousmenu = Compteur;
+}
+
+rotation();
+
+}
+
+void encodeurTemp (){
+  //limite les réglages entre 0 et 45
+  if(Compteur<0) {
+    Compteur=45;
+  } 
+  if (Compteur>45) {
+    Compteur=0;
+  }
+  
+  rotation ();
+}
+void actionMenu () {
+  //change la variable par son opposé
+  menu_principal != menu_principal ;
+}
+
 void reglagetempMin (){
   int tempMin = ud.tempMin;
-  encodeur_temp();
+  encodeurTemp();
   lcd.print("Température mini");
-  lcd.setcursor(0,1);
-  lcd.print(tempMin)
+  lcd.setCursor(0,1);
+  lcd.print(tempMin);
+  //état actuel du bouton
+  boutonSW = digitalRead(pinSW);
+  //enregistre les paramètres
+  if (boutonSW == LOW){
+    delay (debounce);
+    if (boutonSW == LOW){
+     ud.tempMin = tempMin ;
+     sauvegardeEEPROM();
+    }
+  }
+  
 }
+
 void reglagetempMax (){
+  int tempMax = ud.tempMax;
+  encodeurTemp();
+  lcd.print("Température max");
+  lcd.setCursor(0,1);
+  lcd.print(tempMax);
+  //état actuel du bouton
+  boutonSW = digitalRead(pinSW);
+    //enregistre les paramètres
+  if (boutonSW == LOW){
+    delay (debounce);
+    if (boutonSW == LOW){
+     ud.tempMax = tempMax ;
+     sauvegardeEEPROM();
+    }
+  }
 }
+
 void marche (){
     //commande pour obtenir la température
   sensors.requestTemperatures();
@@ -194,52 +287,47 @@ void marche (){
   lcd.clear();
   
 }
+
 void menu (){
   //remise à false des variables
-  boolean marche_select = false;
-  boolean tempMin_select = false;
-  boolean tempMax_select = false;
+   marche_select = false;
+   tempMin_select = false;
+   tempMax_select = false;
   //Appel fct encodeur menu
   encodeur_menu ();
   if (sousmenu = 0){
     lcd.clear();
     lcd.print("Marche");
   }
+  
   else if (sousmenu = 1)
   {
     lcd.clear();
     lcd.print("Réglage température min");
+    boutonSW = digitalRead(pinSW);
+    //entre dans le réglage
+  if (boutonSW == LOW){
+    delay (debounce);
+    if (boutonSW == LOW){
+     tempMin_select = true;
+     menu_principal = false;
+    }
   }
+  }
+  
   else if (sousmenu = 2)
   {
     lcd.clear();
     lcd.print("réglage température max");
+        boutonSW = digitalRead(pinSW);
+    //entre dans le réglage
+  if (boutonSW == LOW){
+    delay (debounce);
+    if (boutonSW == LOW){
+     tempMax_select = true;
+     menu_principal = false;
+    }
   }
   
 }
-void encodeur_menu () {
-//retour à 2 si compteur<0
-if (compteur<0){
-  compteur = 2;
-}
-//retour à 0 si compteur>2
-else if (compteur>2){
-  compteur = 0;
-  
-}
-else {
-  sousmenu = compteur;
-}
-
-}
-void encodeur_temp (){
-  //limite les réglages entre 0 et 45
-  if(compteur<0) {
-    compteur=45;
-  } 
-  if (compteur>45) {
-    compteur=0;
-  }
-  
-  
 }
